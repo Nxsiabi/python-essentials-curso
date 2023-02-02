26d06323647c2d3c5fb42694cd875eea59820849
Nxsiabi <124235582+Nxsiabi@users.noreply.github.com>
2023-02-02 01:59:09 -0500

    Add files via upload

---
+++ Script 1.py
@@ -0,0 +1,9 @@
+print("Empezando a trabajar con Python \n\nRealizado por: Bruno Montalvo") # Empezando con el comando de print para desplegar texto
+print("Consultando los tipos de valores:\n")
+print("El tipo de dato de 875 es: \n", type(875),"\n") #Dado que se puede anidar funcoines, aquí utilizo la función type para determinar el tipo de valor dentro de la función print paar que todo se muestre en pantalla
+print("El tipo de dato de 4.89 es: \n", type(4.89),"\n")
+print("El tipo de dato del texto: Now is better than never. es: \n", type("Now is better than never."),"\n")
+print("El tipo de dato de 1.32 es: \n", type(875),"\n")
+print("¿El valor 5 + 8i corresponde a un valor entero? \n", isinstance(5 + 8j, int),"\n") #Aquí en vez de usar la función type utilizo isinstance para determinar si el valor es el de la pregunta
+print("¿El valor 8.2 corresponde a un valor entero? \n", isinstance(8.2, int),"\n")
+print("¿El texto: Readability counts. corresponde a un string? \n", isinstance("Readability counts", str),"\n")
---
+++ Script 2.py
@@ -0,0 +1,2 @@
+variable=input("Ingreso: ",) #input convierte todo en str entonces no funciona para el script que pide la actividad
+print(type(variable))
